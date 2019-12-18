f5519c33519f4898cbfa0b60a861b5506ca5e4c7
Yvette Wu <dwu415@gmail.com>
2019-12-17 16:22:12 -0800

    improved security

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # <int:pk> means that django expects an integer value and will transfer
     # it to a view as a variable called pk.
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
